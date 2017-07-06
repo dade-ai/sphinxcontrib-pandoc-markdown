47bd1a2dfa6fcc6601abbb03dc4a9b49e84bebcb
dade.ai <dade.ai@kakaobrain.com>
2017-07-06 14:49:51 +0900

    link bug fixed

--- sphinxcontrib/remove_caption_filter.py
+++ sphinxcontrib/remove_caption_filter.py
@@ -16,11 +16,22 @@ Convert as below:
 """
 import os
 from pandocfilters import toJSONFilter, Image, Link, RawInline
+import logging
 
 try:
     import urlparse
 except ImportError:
     from urllib.parse import urlparse
+
+log = logging.getLogger(os.path.basename(__file__))
+
+
+def configure_logging(f):
+    format_string = "%(name)s: [%(levelname)s] %(message)s"
+    logging.basicConfig(format=format_string, level=logging.DEBUG)
+    log.debug("Debug logging enabled.")
+
+    return f
 
 
 def remove_caption_filter(key, value, format_, meta):
@@ -46,15 +57,22 @@ def remove_caption_filter(key, value, format_, meta):
         """
         markdown:
             [node](node_api.md)
-
+            [](README.md)
         json:
             [[u'', [], []], [{u'c': u'node', u't': u'Str'}], [u'node_api.md', u'']]
+            [[u'', [], []], [], [u'README.md', u'']]
         """
         link_path = value[2][0]
         if not bool(urlparse.urlparse(link_path).netloc):
             name, ext = os.path.splitext(link_path)
             if ext and ext.lower() == ".md":
-                link_name = value[1][0]["c"]
+
+                log.info('value: {}'.format(value))
+                if value[1]:
+                    link_name = value[1][0]["c"]
+                else:
+                    link_name = None
+
                 if link_name:
                     return RawInline(u'rst', u':doc:`{} <{}>`'.format(link_name, name))
                 return RawInline(u'rst', u':doc:`{}`'.format(name))
@@ -63,4 +81,4 @@ def remove_caption_filter(key, value, format_, meta):
 
 
 if __name__ == "__main__":
-    toJSONFilter(remove_caption_filter)
+    toJSONFilter(configure_logging(remove_caption_filter))
